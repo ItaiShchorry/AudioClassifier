69f5ddfb8480407d953213d36fa8a61df183719e
Itai Shchorry <itai.shchorry@optimalplus.com>
2021-05-03 13:44:50 +0300

    Added class-level performance & hearing audio capabilities

--- AudioClassifier/vc_notebooks/inference.py
+++ AudioClassifier/vc_notebooks/inference.py
@@ -38,22 +38,22 @@ def extract_mfcc_features(filepath):
         mfccsscaled = np.mean(mfccs.T,axis=0)
         
     except Exception as e:
-        print("Error encountered while parsing file: ", filepath)
+        st.write("Error encountered while parsing file: ", filepath)
         return None 
      
-    return mfccsscaled
+    return audio, sample_rate, mfccsscaled
 
 def from_audio_to_table(uploaded_wav_file):
-    data = extract_mfcc_features(uploaded_wav_file)
+    audio, sample_rate, data = extract_mfcc_features(uploaded_wav_file)
     idx = uploaded_wav_file.name
 
     # Convert into a Panda dataframe 
     X = pd.DataFrame([data], columns=['mfcc_'+str(i) for i in range(data.shape[0])],index=[idx])
 
-    return X
+    return audio, sample_rate, X
 
 def predict_wav(uploaded_wav_file, learn, config):
-    wave_df = from_audio_to_table(uploaded_wav_file)
+    audio, sample_rate, wave_df = from_audio_to_table(uploaded_wav_file)
 
     # update config according to currently used model. Should probably happen outside this method
     run_config = config.copy()
@@ -67,7 +67,7 @@ def predict_wav(uploaded_wav_file, learn, config):
     clas = learn.dls.vocab[clas]
     probs = pd.DataFrame({'class': [learn.dls.vocab[i] for i in range(probs.shape[0])], 'probability': probs}).sort_values('probability', ascending=False)
     
-    return probs.head()# wave_df_proc, clas, probs
+    return audio, sample_rate, probs.head()# wave_df_proc, clas, probs
 
 def load_model(path):
     learn_inf = load_learner(path/'export.pkl')
@@ -102,7 +102,7 @@ def load_model(path):
 # + Collapsed="false"
 import streamlit as st
 def choose_model_helper(text):
-    st.write("## Choosing population")  
+    st.write("### Choosing population")  
     model_name = st.selectbox(
         text,
          MODELS_LST)
@@ -112,7 +112,7 @@ def choose_model_helper(text):
     learn_inf, config = load_model(model_folder)
     feat_imp = picklel(model_folder/'feat_imp.p')
     
-    return learn_inf, config, feat_imp
+    return learn_inf, config, feat_imp, model_name
 
 
 # + Collapsed="false"
@@ -134,11 +134,22 @@ def get_plotting_dict():
     return plotting_dict, kinds
 
 # + Collapsed="false"
+# perf_dfs = []
+# for m in MODELS_LST:
+#     m_path = CWD.parent/'models'/'tabular'
+#     perf_dfs.append(picklel(m_path/m/'metrics_df.p'))
 
+# perf_dfs = pd.concat(perf_dfs)
 
-# + Collapsed="false"
+# glob_stats = ['tr_acc','val_acc','test_acc']
+
+# cs = list(set(perf_dfs.columns) - set(glob_stats))
+# sns.relplot(data=perf_dfs[cs], kind='line')
+
+# + Collapsed="false" jupyter={"outputs_hidden": true}
 import streamlit as st
 from scipy.cluster.hierarchy import dendrogram, linkage, fcluster
+import IPython.display as ipd
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
 # home view
@@ -154,7 +165,7 @@ if selection == "Data Explorator":
     sub_sections = ['Data Summary', 'Target Exploration', 'Correlations']
     internal_selection = st.sidebar.radio("Data Explorator", sub_sections)
     
-    learn_inf, config, _ = choose_model_helper('Which dataset would you like to examine?')
+    learn_inf, config, _, _ = choose_model_helper('Which dataset would you like to examine?')
     df = read_df_from_path(config['FILE_PATH'])
     
     if internal_selection == 'Data Summary':
@@ -213,25 +224,40 @@ if selection == "Model Evaluator":
 
     perf_dfs = pd.concat(perf_dfs)
 
-    st.write("## Models Performance")  
-    st.write(perf_dfs)    
+    st.write("## General Performance")  
+    glob_stats = ['tr_acc','val_acc','test_acc']
+    st.write(perf_dfs[['tr_acc','val_acc','test_acc']])
+    
+    st.write("## Class-level Performance")
+    cs = list(set(perf_dfs.columns) - set(glob_stats))
+    sns.relplot(data=perf_dfs[cs], kind='line')
+    st.pyplot()
 
     st.write("## Model Drilldown") 
     # choose model to evaluate
-    learn_inf, config, feat_imp = choose_model_helper('Which model would you like to investigate?')
+    learn_inf, config, feat_imp, _ = choose_model_helper('Which model would you like to investigate?')
     st.write("### Feature Importance") 
     st.write("implemented via permutation importance methodology") 
     st.write(feat_imp)
 
 if selection == "Inferencer":
-    learn_inf, config, _ = choose_model_helper('Which model would you like to predict with?')
+    learn_inf, config, _, model_name = choose_model_helper('Which model would you like to predict with?')
     
     st.write("## Inference")
     uploaded_wav = st.file_uploader('upload a wave file for prediction')
 
     if uploaded_wav is not None:
-        st.write("Probabilities are:")
-        st.write(predict_wav(uploaded_wav, learn_inf, config))
+        
+        audio, sample_rate, probs = predict_wav(uploaded_wav, learn_inf, config)
+        
+        
+        st.write("You uploaded this sound")
+        html_object = ipd.Audio(audio, rate=sample_rate)
+        print(type(html_object))
+        st.components.v1.html(html_object._repr_html_())
+        
+        st.write(f"And the classification probabilities using {model_name} are:")
+        st.write(probs)
 
     
 ############################### Reference Code ###############################    
@@ -267,6 +293,14 @@ if selection == "Inferencer":
 #     plt.plot(values[:, i])
 
 # st.pyplot(fig)
+
+# + [markdown] Collapsed="false"
+# ### Backlog
+# - Enable training
+#     - interactive preprocessing:
+#         - expose preprocessing configuration
+#         - outlier detection thresholder
+#         - 
 
 # + [markdown] Collapsed="false"
 # ### MadeWithML
